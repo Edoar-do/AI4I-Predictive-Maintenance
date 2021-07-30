3d826f51592076f471750b4687dabfbba43efafa
Edoar_do <edokoppola@gmail.com>
2021-07-30 17:35:57 +0200

    carica tree/svm pesati e sampling

--- mldm_project.py
+++ mldm_project.py
@@ -1,22 +1,24 @@
 #IMPORTAZIONI VARIE
 import sys
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
-import sklearn as skl
 import numpy as np
 import seaborn as sns
 from graphviz import Source
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.metrics import confusion_matrix, mean_squared_error
-from sklearn.datasets import load_iris
+from sklearn.metrics import confusion_matrix
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import os
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
 from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 
+#os.environ["PATH"] += os.pathsep + 'C:/Utenti/jed/Anaconda3/envs/keras/Library/bin/graphviz/'
 #LETTURA DATASET
 dataset = pd.read_csv('ai4i2020.csv')
 
@@ -44,37 +46,30 @@ last = dataset.iloc[:, -1]
 dataset = dataset.drop('Machine failure', axis = 1)
 dataset = dataset.join(one_hot)
 dataset = dataset.join(last)
-#last.replace({1: 'rotto', 0: 'non rotto'}, inplace=True)
-
 
 #CORRELAZIONI TRA FEATURE
-pd.plotting.scatter_matrix(dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False), figsize=(14,22))
-plt.show()
+
+#pd.plotting.scatter_matrix(dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False), figsize=(14,22))
+#plt.show()
+
 #sembra esserci una correlazione tra air temperature e process temperature e infatti è così sicché process temperature è
 #calcolata a partire da air temperature e una correlazione inversa tra rotational speed e torque (infatti sono inversamente proporzionali)
 
-#print(np.corrcoef(dataset['Air temperature [K]'], dataset['Process temperature [K]'])) #-->0.87
-#print(np.corrcoef(dataset['Rotational speed [rpm]'], dataset['Torque [Nm]'])) #-->-0.87
-
-plt.figure(figsize=(16, 6))
-heatmap = sns.heatmap(dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False).corr(), vmin=-1, vmax=1, annot=True)
-heatmap.set_title('Correlation Heatmap')
-plt.show()
-
+#plt.figure(figsize=(16, 6))
+#heatmap = sns.heatmap(dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False).corr(), vmin=-1, vmax=1, annot=True)
+#heatmap.set_title('Correlation Heatmap')
+#plt.show()
 
 #visualizzazione per cercare eventuali outliers
 #Avendo i record più di 3 dimensioni, tutte importanti per individuarli, non possono essere stampati
 #Analizzo le eventuali anomalie nelle singole feature
 
-dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False).plot(subplots=True, figsize=(16,6))
-plt.show()
+#dataset.drop(['L', 'M', 'H', 'Machine failure'], axis=1, inplace=False).plot(subplots=True, figsize=(16,6))
+#plt.show()
 #--> non ci sono anomalie nei valori delle singole feature
 
 
 #############################################
-
-#INSERIRE QUI CHIAMATE A FUNZIONI CHE SI OCCUPANO DELL'ADDESTRAMENTO E DELLA VALUTAZIONI DELLE PRESTAZIONI DI TUTTI I MODELLI. (QUESTIONE DI ORDINE (MENTALE ANCHE))
-
 
 # ADDESTRAMENTO DI ALCUNI MODELLI
 
@@ -100,9 +95,9 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
 
 np.random.seed(42)
 
-def decision_tree(dataset):
+def decision_treeWeighted(dataset):
 
-# draw tree
+    # draw tree
 
     def draw_tree(filename, tree, feature_names, class_names):
         export_graphviz(
@@ -116,71 +111,117 @@ def decision_tree(dataset):
 
         Source.from_file(os.path.join(IMAGES_PATH, filename)).render(view = True)
 
-# Costruisco il dataset negli input e nei target che serviranno per l'addestramento
+    # Costruisco il dataset negli input e nei target che serviranno per l'addestramento
 
-    dataset.drop(['L', 'M', 'H'], axis = 1, inplace = True) # non venivano mai considerati nell'albero (ce ne siamo accorti dopo alcune prove)
+    dataset = dataset.drop(['L', 'M', 'H'], axis = 1) # non venivano mai considerati nell'albero (ce ne siamo accorti dopo alcune prove)
     X = dataset.iloc[:, :-1]
     Y = dataset.iloc[:, -1]
 
-# split data
+    # split data
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size = 0.65, random_state = 42)
 
-# albero di decisione 
-
-# euristica per i pesi dato lo sbilanciamento del dataset
+    # albero di decisione
+    # euristica per i pesi dato lo sbilanciamento del dataset
 
     nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
     ratio = nonfailure_count / failure_count
     weights = {0: 1.0, 1: ratio}
 
-# search with grid_search_cv
+    tree = DecisionTreeClassifier(max_depth = 4, class_weight = weights, criterion = 'entropy', random_state=42, min_samples_leaf=30)
+    dizionario = cross_validate(tree, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
 
-    params = {'min_samples_leaf': [30, 40, 50]}
-    cv = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 3, random_state = 42)
-    grid_search_cv = GridSearchCV(DecisionTreeClassifier(max_depth = 4, class_weight = weights, criterion = 'entropy', random_state=42), params, verbose=1, cv = cv, scoring='f1_weighted')
-    grid_search_cv.fit(X_train, Y_train)
-    draw_tree("tree_pm_grid_search.dot", grid_search_cv.best_estimator_, dataset.columns.values[:-1], ['Non rotto', 'Rotto'])
-
-# test prestazioni
-
-    Y_pred = grid_search_cv.predict(X_test)
-    print('Accuracy score of decisione tree: ', accuracy_score(Y_test, Y_pred))
-    print('Best params after grid search applied to decision tree: ', grid_search_cv.best_params_)
-
-
+    Y_pred = dizionario['estimator'][0].predict(X_test)
     tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
     print("TrueNegative: %d" % tn)
     print("FalsePostive: %d" % fp)
     print("FalseNegative: %d" % fn)
     print("TruePositive: %d" % tp)
 
+    #Risultati ottenuti con cross validazione del modello (PESATO) e calcolo delle prestazioni con matrice di confusione su dati di testing precedentemente separati:
+    #Recall circa del 5.7% (sui valori '1') -> valore non eccezionale dato il contesto industriale
+    #Specificity circa del 9.34
 
-# I nostri risultati per l'albero di decisione sono:
-# Il dataset è fortemente sbilanciato a favore delle istanze negative (il 3.39 % delle istanze sono positive).
-# Per questo motivo, abbiamo deciso di pesare i dati di input (utilizzando come euristica per i pesi il rapporto tra il 
-# numero di istanze negative e il numero di istanze positive). Inoltre abbiamo utilizzato una score F2 per dare maggiore importanza ai falsi negativi, problema grave in ambito industriale
-# e di manutenzione predittiva
-# Dopodiché abbiamo usato RepeteadStratifiedKFold passato come parametro a una GridSearchCV per la crossvalidazione a caccia della migliore configurazione possibile di iperparametri
-# Questo ci ha permesso di determinare in maniera automatica alcuni degli iperparametri.
-# L'accuracy score è circa di 0.89.
-# fn e tp erano circa 5 e 100, quindi un risultato buono considerando quanto è sbilanciato il dataset.
-
-# Support vector machines
-
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-
-def support_vector_machines(dataset):
-    # prepara test e training data
+def svm_weighted(dataset):
+    dataset = dataset.drop(['L', 'M', 'H'], axis=1)  # non venivano mai considerati nell'albero (ce ne siamo accorti dopo alcune prove)
     X = dataset.iloc[:, :-1]
     Y = dataset.iloc[:, -1]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size = 0.65, random_state = 42)
 
-    tuned_parameters = [{'C': [0.01, 0.1, 1, 10, 100],
-                         'gamma': [0.5, 1,2,3,4]}]
-    cv = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 3, random_state = 42)
-    clf = GridSearchCV(SVC(kernel='rbf'), tuned_parameters, cv=10, scoring='accuracy')
-    clf.fit(X_train, Y_train)
-    print(clf.best_params_)
+    # split data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
 
-support_vector_machines(dataset)
+    # albero di decisione
+    # euristica per i pesi dato lo sbilanciamento del dataset
+
+    nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
+    ratio = nonfailure_count / failure_count
+    weights = {0: 1.0, 1: ratio}
+
+    svm = SVC(kernel='rbf', class_weight=weights, C=float('inf'), random_state=42)
+    dizionario = cross_validate(svm, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+    #risultati ottenuti con svm con kernel gaussiano e soft margin molto limitato, modello cross-validato e prestazioni misurati su un testing set pre-separato.
+
+def svm_sampling(dataset):
+    dataset = dataset.drop(['L', 'M', 'H'], axis=1)
+    X = dataset.iloc[:, :-1]
+    Y = dataset.iloc[:, -1]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
+
+    svm = SVC(kernel='rbf', C=float('inf'), random_state=42)
+    over = SMOTE(sampling_strategy=0.45, random_state=42)
+    under = RandomUnderSampler(sampling_strategy=0.5, random_state=42)
+    steps = [('o', over), ('u', under), ('model', svm)]
+    pipeline = Pipeline(steps=steps)
+
+    dizionario = cross_validate(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+def decisionTree_sampling(dataset):
+    dataset = dataset.drop(['L', 'M', 'H'], axis=1)
+    X = dataset.iloc[:, :-1]
+    Y = dataset.iloc[:, -1]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
+
+
+    tree = DecisionTreeClassifier(criterion='entropy', max_depth=4, min_samples_leaf=30)
+    over = SMOTE(sampling_strategy=0.45, random_state=42)
+    under = RandomUnderSampler(sampling_strategy=0.5, random_state=42)
+    steps = [('o', over), ('u', under), ('model', tree)]
+    pipeline = Pipeline(steps=steps)
+
+    #scores = cross_val_score(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
+    dizionario = cross_validate(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+    # Risultati ottenuti con cross validazione del modello allenato su over e undersampling e calcolo delle prestazioni con matrice di confusione su dati di testing precedentemente separati:
+    # Recall circa del 8.7% (sui valori '1') -> valore non eccezionale dato il contesto industriale
+    # Specificity circa del 9.34
+    #Siccome quello che conta è la recall e non la specificity in questo contesto, l'albero di decisione coi pesi è migliore di quello con over-under sampling
+
+print('SVM PESATO')
+svm_weighted(dataset)
+print('SVM CON SAMPLING')
+svm_sampling(dataset)
+#print('DECISION TREE PESATO')
+#decision_treeWeighted(dataset)
+#print('DECISION TREE WITH SAMPLING')
+#decisionTree_sampling(dataset)
