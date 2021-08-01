51301e8edd08fe39b76ad4113ab5bc324efe180e
Edoar_do <edokoppola@gmail.com>
2021-08-01 17:31:09 +0200

    random forest pesata,random forest sampling,  feature importance

--- mldm_project.py
+++ mldm_project.py
@@ -6,7 +6,7 @@ import numpy as np
 import seaborn as sns
 from graphviz import Source
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, BaggingClassifier, RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
@@ -123,12 +123,12 @@ def decision_treeWeighted(dataset):
     # albero di decisione
     # euristica per i pesi dato lo sbilanciamento del dataset
 
-    nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
-    ratio = nonfailure_count / failure_count
-    weights = {0: 1.0, 1: ratio}
+    #nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
+    #ratio = nonfailure_count / failure_count
+    #weights = {0: 1.0, 1: ratio}
 
-    tree = DecisionTreeClassifier(max_depth = 4, class_weight = weights, criterion = 'entropy', random_state=42, min_samples_leaf=30)
-    dizionario = cross_validate(tree, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+    tree = DecisionTreeClassifier(max_depth = 4, class_weight = 'balanced', criterion = 'entropy', random_state=42, min_samples_leaf=30)
+    dizionario = cross_validate(tree, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
 
     Y_pred = dizionario['estimator'][0].predict(X_test)
     tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
@@ -149,15 +149,10 @@ def svm_weighted(dataset):
     # split data
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
 
-    # albero di decisione
     # euristica per i pesi dato lo sbilanciamento del dataset
 
-    nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
-    ratio = nonfailure_count / failure_count
-    weights = {0: 1.0, 1: ratio}
-
-    svm = SVC(kernel='rbf', class_weight=weights, C=float('inf'), random_state=42)
-    dizionario = cross_validate(svm, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+    svm = SVC(kernel='rbf', C=float('inf'), gamma=1, class_weight='balanced', random_state=42)
+    dizionario = cross_validate(svm, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
 
     Y_pred = dizionario['estimator'][0].predict(X_test)
     tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
@@ -166,7 +161,7 @@ def svm_weighted(dataset):
     print("FalseNegative: %d" % fn)
     print("TruePositive: %d" % tp)
 
-    #risultati ottenuti con svm con kernel gaussiano e soft margin molto limitato, modello cross-validato e prestazioni misurati su un testing set pre-separato.
+    #recall=0 e specificity=3394/3395 -> risultati non accettabili
 
 def svm_sampling(dataset):
     dataset = dataset.drop(['L', 'M', 'H'], axis=1)
@@ -174,13 +169,13 @@ def svm_sampling(dataset):
     Y = dataset.iloc[:, -1]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
 
-    svm = SVC(kernel='rbf', C=float('inf'), random_state=42)
+    svm = SVC(kernel='rbf', C=float('inf'), gamma=1, class_weight='balanced', random_state=42)
     over = SMOTE(sampling_strategy=0.45, random_state=42)
     under = RandomUnderSampler(sampling_strategy=0.5, random_state=42)
-    steps = [('o', over), ('u', under), ('model', svm)]
-    pipeline = Pipeline(steps=steps)
+    X_train, Y_train = over.fit_resample(X_train, Y_train)
+    X_train, Y_train = under.fit_resample(X_train, Y_train)
 
-    dizionario = cross_validate(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+    dizionario = cross_validate(svm, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
 
     Y_pred = dizionario['estimator'][0].predict(X_test)
     tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
@@ -188,6 +183,8 @@ def svm_sampling(dataset):
     print("FalsePostive: %d" % fp)
     print("FalseNegative: %d" % fn)
     print("TruePositive: %d" % tp)
+
+    # recall = 2/105 e specificity = 3391/3395 -> risultati terribilmente non accettabili
 
 def decisionTree_sampling(dataset):
     dataset = dataset.drop(['L', 'M', 'H'], axis=1)
@@ -196,14 +193,14 @@ def decisionTree_sampling(dataset):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
 
 
-    tree = DecisionTreeClassifier(criterion='entropy', max_depth=4, min_samples_leaf=30)
+    tree = DecisionTreeClassifier(criterion='entropy', max_depth=4, min_samples_leaf=30, class_weight='balanced')
     over = SMOTE(sampling_strategy=0.45, random_state=42)
     under = RandomUnderSampler(sampling_strategy=0.5, random_state=42)
     steps = [('o', over), ('u', under), ('model', tree)]
     pipeline = Pipeline(steps=steps)
 
     #scores = cross_val_score(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
-    dizionario = cross_validate(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='f1_weighted', return_estimator=True)
+    dizionario = cross_validate(pipeline, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
 
     Y_pred = dizionario['estimator'][0].predict(X_test)
     tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
@@ -213,15 +210,126 @@ def decisionTree_sampling(dataset):
     print("TruePositive: %d" % tp)
 
     # Risultati ottenuti con cross validazione del modello allenato su over e undersampling e calcolo delle prestazioni con matrice di confusione su dati di testing precedentemente separati:
-    # Recall circa del 8.7% (sui valori '1') -> valore non eccezionale dato il contesto industriale
-    # Specificity circa del 9.34
+    # Recall circa del 8.6% (sui valori '1') -> valore non eccezionale dato il contesto industriale
+    # Specificity circa del 9.2%
     #Siccome quello che conta è la recall e non la specificity in questo contesto, l'albero di decisione coi pesi è migliore di quello con over-under sampling
 
-print('SVM PESATO')
-svm_weighted(dataset)
-print('SVM CON SAMPLING')
-svm_sampling(dataset)
-#print('DECISION TREE PESATO')
-#decision_treeWeighted(dataset)
-#print('DECISION TREE WITH SAMPLING')
-#decisionTree_sampling(dataset)
+def randomForest_weighted(dataset):
+    dataset = dataset.drop(['L', 'M', 'H'], axis=1)
+    X = dataset.iloc[:, :-1]
+    Y = dataset.iloc[:, -1]
+
+    # split data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
+
+    randomForest = RandomForestClassifier(n_jobs=-1, criterion='entropy', n_estimators=1000, class_weight='balanced_subsample',
+                                         random_state=42, max_samples=0.5, max_leaf_nodes=16)
+    dizionario = cross_validate(randomForest, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+    #recall 10/105, specificity=3118/3118+277
+
+def samples_randomForest(dataset):
+    dataset = dataset.drop(['L', 'M', 'H'], axis=1)
+    X = dataset.iloc[:, :-1]
+    Y = dataset.iloc[:, -1]
+
+    # split data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
+
+    randomForest = RandomForestClassifier(n_jobs=-1, criterion='entropy', n_estimators=1000,random_state=42,
+                                          max_samples=0.5, max_leaf_nodes=16, class_weight='balanced_subsample')
+    over = SMOTE(sampling_strategy=0.45, random_state=42)
+    under = RandomUnderSampler(sampling_strategy=0.5, random_state=42)
+    X_train, Y_train = over.fit_resample(X_train, Y_train)
+    X_train, Y_train = under.fit_resample(X_train, Y_train)
+
+    dizionario = cross_validate(randomForest, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+    for name, score in zip(dataset.columns, dizionario['estimator'][0].feature_importances_):
+        print(name, score)
+
+    #recall = 5/105, specificity=3039/3039+356 MODELLO MIGLIORE MAI VISTO!!!!
+    #The “balanced_subsample” mode is the same as “balanced” except that weights are computed based on the bootstrap sample for every tree grown.
+
+def decisionTreeWithMostImportantFeature(dataset):
+    # draw tree
+
+    def draw_tree(filename, tree, feature_names, class_names):
+        export_graphviz(
+            tree,
+            out_file=os.path.join(IMAGES_PATH, filename),
+            feature_names=feature_names,
+            class_names=class_names,
+            rounded=True,
+            filled=True
+        )
+
+        Source.from_file(os.path.join(IMAGES_PATH, filename)).render(view=True)
+
+    # Costruisco il dataset negli input e nei target che serviranno per l'addestramento
+
+    dataset = dataset.drop(['L', 'M', 'H', 'Air temperature [K]', 'Process temperature [K]'],axis=1)  # non venivano mai considerati nell'albero (ce ne siamo accorti dopo alcune prove)
+    X = dataset.iloc[:, :-1]
+    Y = dataset.iloc[:, -1]
+
+    # split data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.65, random_state=42)
+
+    # albero di decisione
+    # euristica per i pesi dato lo sbilanciamento del dataset
+
+    # nonfailure_count, failure_count = dataset.groupby('Machine failure').size()
+    # ratio = nonfailure_count / failure_count
+    # weights = {0: 1.0, 1: ratio}
+
+    tree = DecisionTreeClassifier(max_depth=4, class_weight='balanced', criterion='entropy', random_state=42,
+                                  min_samples_leaf=30)
+    dizionario = cross_validate(tree, X_train, Y_train, cv=5, n_jobs=-1, scoring='recall', return_estimator=True)
+
+    Y_pred = dizionario['estimator'][0].predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(Y_test, Y_pred).ravel()
+    print("TrueNegative: %d" % tn)
+    print("FalsePostive: %d" % fp)
+    print("FalseNegative: %d" % fn)
+    print("TruePositive: %d" % tp)
+
+    # recall=3.81%, specificity=17.67%
+    # grazie alla random forest con samples abbiamo trovato le feature più importanti e adesso le abbiamo usate per allenare un modello semplicissimo come un albero
+    # per vedere se ci sono stati miglioramenti delle prestazioni e così è stato.
+
+
+# print('SVM PESATO')
+# svm_weighted(dataset)
+# print()
+# print('SVM CON SAMPLING')
+# svm_sampling(dataset)
+# print()
+# print('DECISION TREE PESATO')
+# decision_treeWeighted(dataset)
+# print()
+# print('DECISION TREE WITH SAMPLING')
+# decisionTree_sampling(dataset)
+# print()
+# print('random forest pesata')
+# randomForest_weighted(dataset)
+# print()
+# print('random forest sampling')
+# samples_randomForest(dataset)
+# print()
+# print('most important features weightede decision tree')
+# decisionTreeWithMostImportantFeature(dataset)
+# print()
